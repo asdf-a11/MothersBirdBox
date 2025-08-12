17d56752d0f495dfa5a5e6e8a41d7d9ea2242ded
asdf <73710151+asdf-a11@users.noreply.github.com>
2025-08-12 21:26:33 +0100

    Fixes10

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ try:
             def GetFrame(self):
                 # Create an in-memory stream
                 #my_stream = io.BytesIO()
-                buffer = self.cam.capture_buffer().tobytes()
+                buffer = self.cam.capture_buffer("main").tobytes()
                 with open("test.jpeg", "wb") as f:
                     f.write(buffer)
                 print("returning buffer,", type(buffer))
