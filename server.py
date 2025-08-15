f6a71fffe675ef5287624cc9e27ead5ccec3835a
asdf <73710151+asdf-a11@users.noreply.github.com>
2025-08-15 17:24:28 +0100

    DebugFalse

--- server.py
+++ server.py
@@ -103,7 +103,7 @@ try:
 
 
     if __name__ == '__main__':
-        app.run(host='0.0.0.0')
+        app.run(host='0.0.0.0', debug=False)
     '''
     from flask import Flask, render_template, Response
 
