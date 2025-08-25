1c2f95282f26bb937aa2ea12b84c66642e3c9768
asdf <73710151+asdf-a11@users.noreply.github.com>
2025-08-25 01:03:43 +0100

    ImproveLoadTimes

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import traceback
 try:
-    # save this as app.py
-    
     from flask import Flask, render_template, Response, request, jsonify
     import io
     import time
@@ -9,6 +7,8 @@ try:
     import RPi.GPIO as GPIO
     import time
     from threading import Thread
+    print("Importing camera modual")
+    from picamera2 import Picamera2
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     #import cv2
@@ -16,17 +16,6 @@ try:
     oldBrightness = brightness
     ledPinList = [5,6]
 
-    '''
-    class ModulateLED():
-        def __init__(self, pinNumber, brightness):
-            self.pinNumber = pinNumber
-            self.brightness = brightness
-        def Start(self):
-            GPIO.setup(6,GPIO.OUT)
-            GPIO.output(6,GPIO.HIGH)
-        def Update(self, newBrightness):
-            pass
-    '''
     def ModulateLED(pin):
         SCALE = 10**-3
         while 1:
@@ -56,69 +45,44 @@ try:
 
     app = Flask(__name__)
 
-    if False:
-        class Camera():
-            def __init__(self):
-                self.imgList = [open(f"{i}.jpg", "rb").read() for i in range(1,3)]
-                self.imgCounter = 0
-            def GetFrame(self):
-                print("Getting frame")
-                img = self.imgList[self.imgCounter]
-                self.imgCounter += 1
-                if self.imgCounter >= len(self.imgList):
-                    self.imgCounter = 0
-                return img
-    else:
-        class Camera():
-            def __init__(self): 
-                from picamera2 import Picamera2
-                self.cam = Picamera2()
-                #config = self.cam.create_preview_configuration({"format": "MJPEG"})#M
-                #self.cam.configure(config)
-                #self.cam.configure(self.cam.create_still_configuration())
-                #self.cam.start()
-                self.cam.stop()
-                self.cam.configure(
-                    self.cam.create_still_configuration(main={"size": (1280,720)})
-                )
-                self.cam.set_controls({
-                    "AfMode": 2,
-                    "AfTrigger": 0  # Optional: triggers the autofocus cycle
-                })
-                self.cam.start()
-                self.oldBrightness = brightness
-            def GetFrame(self):
-                # Capture as RGB array
-                array = self.cam.capture_array()
-
-                # Convert to JPEG bytes
-                image = Image.fromarray(array)
-                jpeg_io = io.BytesIO()
-                image.save(jpeg_io, format="JPEG")
-                #self.cam.stop()
-
-
-                return jpeg_io.getvalue()
-
+    class Camera():
+        def __init__(self): 
+            self.cam = Picamera2()
+            self.cam.stop()
+            self.cam.configure(
+                self.cam.create_still_configuration(main={"size": (1280,720)})
+            )
+            self.cam.set_controls({
+                "AfMode": 2,
+                "AfTrigger": 0  # Optional: triggers the autofocus cycle
+            })
+            self.cam.start()
+            self.oldBrightness = brightness
+        def GetFrame(self):
+            # Capture as RGB array
+            array = self.cam.capture_array()
+            # Convert to JPEG bytes
+            image = Image.fromarray(array)
+            jpeg_io = io.BytesIO()
+            image.save(jpeg_io, format="JPEG")
+            return jpeg_io.getvalue()
+    
+    print("Creating camera")
+    cam = Camera()
 
     @app.route("/")
     def hello():
-        return open("index.html","r",encoding="utf-8").read()#render_template("index.html")
+        return open("index.html","r",encoding="utf-8").read()
 
     def gen(camera):
         while True:
-            #print("\n\n\n here \n\n\n")
             frame = camera.GetFrame()
-            #print(type(frame))
-            #input()
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
- 
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
 
     @app.route('/video_feed')
     def video_feed():
-        return Response(gen(Camera()),
+        return Response(gen(cam),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
 
     @app.route('/brightness', methods=['POST'])
