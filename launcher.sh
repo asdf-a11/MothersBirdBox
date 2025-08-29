61d8ca43505ca1166282770ef5c2ce98862574e6
asdf <73710151+asdf-a11@users.noreply.github.com>
2025-08-29 17:12:01 +0100

    AddLauncher

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd ~/MothersBirdBox
+echo "Starting python server from launcher.sh"
+python3 server.py
+cd ~/
+cd /
